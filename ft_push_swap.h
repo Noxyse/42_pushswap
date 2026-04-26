08092e0baea22c5d1dce1f5bbb8d3f91be09c5c0
Mattieu Gedeon <mgedeon@student.42belgium.be>
2026-04-26 14:57:57 +0200

    change of name

--- ft_push_swap.h
+++ ft_push_swap.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ft_push_swap.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgedeon <mgedeon@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 14:07:38 by mgedeon           #+#    #+#             */
-/*   Updated: 2026/04/26 14:56:01 by mgedeon          ###   ########.fr       */
+/*   Updated: 2026/04/26 14:57:45 by mgedeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H	
-# define PUSH_SWAP_H	
+#ifndef FT_PUSH_SWAP_H	
+# define FT_PUSH_SWAP_H	
 
 typedef struct s_node
 {
