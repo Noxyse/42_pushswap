31ace12b2bc7fe2628317d7a82a939ed9fa94d45
Mattieu Gedeon <mgedeon@student.42belgium.be>
2026-04-28 12:53:23 +0200

    boop

--- rules.c
+++ rules.c
@@ -6,7 +6,7 @@
 /*   By: mgedeon <mgedeon@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 12:23:21 by mgedeon           #+#    #+#             */
-/*   Updated: 2026/04/28 11:32:08 by mgedeon          ###   ########.fr       */
+/*   Updated: 2026/04/28 12:53:11 by mgedeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@ void	sa(t_stack *stack_a)
 {
 	void	*tmp;
 
-	if (!stack_a || !stack_a->first || !stack_a->first->next)
+	if (!stack_a || !stack_a->head || !stack_a->head->next)
 		return ;
-	tmp = stack_a->first->content;
-	stack_a->first->content = stack_a->first->next->content;
-	stack_a->first->next->content = tmp;
+	tmp = stack_a->head->content;
+	stack_a->head->content = stack_a->head->next->content;
+	stack_a->head->next->content = tmp;
 }
 
 void	sb(t_stack *stack_b)
 {
 	void	*tmp;
 
-	if (!stack_b || !stack_b->first || !stack_b->first->next)
+	if (!stack_b || !stack_b->head || !stack_b->head->next)
 		return ;
-	tmp = stack_b->first->content;
-	stack_b->first->content = stack_b->first->next->content;
-	stack_b->first->next->content = tmp;
+	tmp = stack_b->head->content;
+	stack_b->head->content = stack_b->head->next->content;
+	stack_b->head->next->content = tmp;
 }
 
 void	ss(t_stack *stack_a, t_stack *stack_b)
@@ -42,22 +42,22 @@ void	ss(t_stack *stack_a, t_stack *stack_b)
 
 void	pa(t_stack *stack_a, t_stack *stack_b)
 {
-	void	*tmp;
+	t_node	*tmp;
 
-	if (!stack_a || !stack_b || !stack_a->first || !stack_b->first)
+	if (!stack_a || !stack_b || !stack_a->head || !stack_b->head)
 		return ;
-	tmp = stack_b->first->content;
-	ft_lstdelone(stack_b->first);
-	ft_lstadd_front(*stack_a, ft_lstnew(tmp));
+	tmp = stack_b->head->content;
+	remove_head(&stack_b->head);
+	stackadd_front(&stack_a->head, tmp);
 }
 
 void	pb(t_stack *stack_a, t_stack *stack_b)
 {
 	void	*tmp;
 
-	if (!stack_a || !stack_b || !stack_a->first || !stack_b->first)
+	if (!stack_a || !stack_b || !stack_a->head || !stack_b->head)
 		return ;
-	tmp = stack_a->first->content;
-	ft_lstdelone(stack_a->first);
+	tmp = stack_a->head->content;
+	ft_lstdelone(stack_a->head);
 	ft_lstadd_front(*stack_b, ft_lstnew(tmp));
 }
