ca74e09e0c7ad4c94a8a45ebf7c59924f44ee961
Mattieu Gedeon <mgedeon@student.42belgium.be>
2026-04-29 14:16:46 +0200

    index

--- ft_push_swap.h
+++ ft_push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mgedeon <mgedeon@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 14:07:38 by mgedeon           #+#    #+#             */
-/*   Updated: 2026/04/29 12:06:17 by mgedeon          ###   ########.fr       */
+/*   Updated: 2026/04/29 14:16:34 by mgedeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FT_PUSH_SWAP_H	
 # define NULL (void *) 0
 
-// ------- STRUCT ------- //
+// ------- STRUCTURES ------- //
 
 typedef struct s_node
 {
@@ -55,7 +55,7 @@ void	rra(t_stack *stack_a);
 void	rrb(t_stack *stack_b);
 void	rrr(t_stack *stack_a, t_stack *stack_b);
 
-// ------- HELPERS ------- //
+// ------- STACK MANIPULATION ------- //
 
 t_node	*ft_stacknew(void *content);
 t_node	*ft_stacklast(t_node *stack);
@@ -64,5 +64,10 @@ t_node	*ft_remove_tail(t_node **stack);
 void	ft_stackdelone(t_node *stack, void (*del)(void *));
 void	ft_stackadd_front(t_node **stack, t_node *new);
 void	ft_stackadd_back(t_node **stack, t_node *new);
+
+// ------- INDEXATION ------- //
+
+static int	get_index(t_node *target_node, t_stack *stack);
+void	ft_value_to_index(t_stack *stack);
 
 #endif
