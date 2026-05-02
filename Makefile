3dfc89d2a0fe71ecec91311e8a03c94f1749a85b
Celian Gremy <ceceoverwatch@gmail.comgit>
2026-05-02 16:53:03 +0200

    MOd + makefile

---
+++ Makefile
@@ -0,0 +1,60 @@
+# ===== NAMES ===== #
+
+NAME = push_swap
+LIBFT_PRINTF = ./printf/libftprintf.a
+
+# ===== COMPILATION ===== #
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+RM = rm -f
+
+# ===== DIRECTORIES AND FILES ===== #
+
+# All your push_swap source files in the root folder
+SRCS = ft_medium_algo.c \
+       ft_parse_to_int.c \
+       ft_push_swap.c \
+       ft_simple_algo.c \
+       ft_value_to_index.c \
+       main.c \
+       push.c \
+       reverse_rotate.c \
+       rotate.c \
+       stack_manipulation.c \
+       stack_manipulation2.c \
+	   ft_strcmp.c \
+       swap.c
+
+OBJS = $(SRCS:.c=.o)
+
+# Includes: current directory (for push_swap.h) and printf includes
+INC = -I. -I./printf/includes -I./printf/libft
+
+# ===== RULES ===== #
+
+all: $(NAME)
+
+# 1. Compile the printf library by calling its Makefile
+# 2. Compile push_swap using the library
+$(NAME): $(OBJS)
+	@make -C ./printf
+	$(CC) $(CFLAGS) $(OBJS) $(LIBFT_PRINTF) -o $(NAME)
+	@echo "push_swap compiled successfully!"
+
+%.o: %.c
+	$(CC) $(CFLAGS) $(INC) -c $< -o $@
+
+clean:
+	@make clean -C ./printf
+	$(RM) $(OBJS)
+	@echo "push_swap objects deleted"
+
+fclean: clean
+	@make fclean -C ./printf
+	$(RM) $(NAME)
+	@echo "push_swap executable deleted"
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- ft_push_swap.c
+++ ft_push_swap.c
@@ -6,11 +6,13 @@
 /*   By: celgremy <celgremy@student.42belgium.be    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 14:48:04 by celgremy          #+#    #+#             */
-/*   Updated: 2026/05/02 16:07:34 by celgremy         ###   ########.fr       */
+/*   Updated: 2026/05/02 16:52:41 by celgremy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
+
+int	ft_strcmp(char *s1, char *s2);
 
 int	ft_compute_disorder(int *a, int nb_arg)
 {
@@ -66,7 +68,7 @@ void	ft_fill_stack(int *tab_a, int nb_param, t_stack *a)
 		val_ptr = malloc(sizeof(int));
 		if (!val_ptr)
 		{
-			ft_stackclear(a);
+			ft_stackclear(a, del);
 			return ;
 		}
 		*val_ptr = tab_a[i];
@@ -78,15 +80,17 @@ void	ft_fill_stack(int *tab_a, int nb_param, t_stack *a)
 	}
 }
 
-int	ft_check_flags(char flags)
+int	ft_check_flags(char *flags)
 {
-	if (ft_strcmp(flags, "--adaptive") == 1)
+	if (!flags)
+		return (-1);
+	else if (ft_strcmp(flags, "--adaptive") == 0)
 		return (0);
-	else if (ft_strcmp(flags, "--simple") == 1)
+	else if (ft_strcmp(flags, "--simple") == 0)
 		return (1);
-	else if (ft_strcmp(flags, "--medium") == 1)
+	else if (ft_strcmp(flags, "--medium") == 0)
 		return (2);
-	else if (ft_strcmp(flags, "--complex") == 1)
+	else if (ft_strcmp(flags, "--complex") == 0)
 		return (3);
 	return (-1);
 }
@@ -96,19 +100,22 @@ void	*ft_push_swap(int **stack_a, int nb_param, char flags)
 	t_stack	*a;
 	t_stack	*b;
 	int		*tab_a;
+	int		res_flags;
 
 	tab_a = *stack_a;
 	a = ft_init_stack();
 	b = ft_init_stack();
-	ft_fill_stack(tab_a, nb_param, &a);
-	if (ft_check_flags == 0)
+	ft_fill_stack(tab_a, nb_param, a);
+	res_flags = ft_check_flags(flags);
+	if (res_flags == 0)
 		ft_compute_disorder(stack_a, nb_param);
-	else if (ft_check_flags == 1)
+	else if (res_flags == 1)
 		ft_simple_algo(a, b);
-	else if (ft_check_flags == 2)
+	else if (res_flags == 2)
 		ft_medium_algo(a, b);
-	else if (ft_check_flags == 3)
+	else if (res_flags == 3)
 		ft_complex_algo(a, b);
 	else
 		ft_printf("Error\n");
+	return (NULL);
 }
