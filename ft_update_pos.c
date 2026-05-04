af61cc6862b39169f3cd4c8c1e2b57f2ec74a958
Mattieu Gedeon <mgedeon@student.42belgium.be>
2026-05-04 14:25:05 +0200

    update pos

--- ft_update_pos.c
+++ ft_update_pos.c
@@ -1,18 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_update_stack.c                                  :+:      :+:    :+:   */
+/*   ft_update_pos.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgedeon <mgedeon@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/04 13:41:13 by mgedeon           #+#    #+#             */
-/*   Updated: 2026/05/04 14:00:10 by mgedeon          ###   ########.fr       */
+/*   Updated: 2026/05/04 14:23:34 by mgedeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 
-ft_update_stack(t_stack *stack)
+void	ft_update_pos(t_stack *stack)
 {
-	
+	t_node	*current;
+	t_data	*data;
+	int		pos;
+
+	pos = 0;
+	current = stack->head;
+	while (current)
+	{
+		data = (t_data *)current->content;
+		data->pos = pos;
+		pos++;
+		current = current->next;
+	}
 }
