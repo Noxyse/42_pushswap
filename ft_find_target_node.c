8f580f6491f58f7efdd86555dafac39f1b52f4c7
Mattieu Gedeon <mgedeon@student.42belgium.be>
2026-05-04 14:46:14 +0200

    find target

---
+++ ft_find_target_node.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_target_node.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgedeon <mgedeon@student.42belgium.be>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/04 14:32:53 by mgedeon           #+#    #+#             */
+/*   Updated: 2026/05/04 14:45:27 by mgedeon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_push_swap.h"
+
+void	ft_find_target_node(t_stack *a, t_stack *b)
+{
+	t_node	*current_a;
+	t_node	*current_b;
+	t_data	*data_a;
+	t_data	*data_b;
+
+	current_a = a->head;
+	current_b = b->head;
+	while (current_b)
+	{
+		data_b = (t_data *)current_b->content;
+		data_b->target_node = NULL;
+		current_b = current_b->next;
+	}
+	while (current_a)
+	{
+		data_a = (t_data *)current_a->content;
+		if (data_a->index > data_b->index)
+		{
+			if (data_b->target_node == NULL || data_a->index < data_b->target_node)
+		}
+	}
+}
